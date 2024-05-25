435cc4648ea64ddcb5a304a623b122304ca24b74
dt4real <dotchere95@gmail.com>
2024-05-25 11:01:14 +0200

    Added Header component

---
+++ src/shared/header/interfaces/header.interface.ts
@@ -0,0 +1,44 @@
+import { Dispatch, SetStateAction } from 'react';
+
+export interface IReduxHeader {
+  type: string;
+  payload: string;
+}
+
+export interface IReduxShowCategory {
+  type: string;
+  payload: boolean;
+}
+
+export interface IReduxNotification {
+  type?: string;
+  payload: INotification;
+}
+
+export interface INotification {
+  hasUnreadMessage?: boolean;
+  hasUnreadNotification?: boolean;
+}
+
+export interface IHeaderSideBarProps {
+  setShowRegisterModal?: Dispatch<SetStateAction<IHeaderModalProps>>;
+  setShowLoginModal?: Dispatch<SetStateAction<IHeaderModalProps>>;
+  setOpenSidebar?: Dispatch<SetStateAction<boolean>>;
+}
+
+export interface IHeader {
+  navClass: string;
+}
+
+export interface ISettings {
+  id: number;
+  name: string;
+  url: string;
+  show: boolean;
+}
+
+export interface IHeaderModalProps {
+  login: boolean;
+  register: boolean;
+  forgotPassword: boolean;
+}
