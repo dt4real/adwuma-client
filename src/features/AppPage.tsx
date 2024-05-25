3add43243e58ba7062c0dd56c189c652572c7313
dt4real <dotchere95@gmail.com>
2024-05-25 10:00:13 +0200

    first commit

---
+++ src/features/AppPage.tsx
@@ -0,0 +1,7 @@
+import { FC, ReactElement } from 'react';
+
+const AppPage: FC = (): ReactElement => {
+  return <div>AppPage</div>;
+};
+
+export default AppPage;
---
+++ src/features/index/Index.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactElement } from 'react';
+
+const Index: FC = (): ReactElement => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Index;
