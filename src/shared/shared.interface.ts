31b3e59460b29ab7752af0f9810e31ed0f564fc4
dt4real <dotchere95@gmail.com>
2024-06-03 01:03:42 +0200

    Add Interfaces

--- src/shared/shared.interface.ts
+++ src/shared/shared.interface.ts
@@ -1,7 +1,13 @@
 import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { ChangeEvent, CSSProperties, Dispatch, KeyboardEvent, ReactNode, SetStateAction } from 'react';
-import { IAuthDocument, IAuthResponse } from 'src/features/auth/interfaces/auth.interface';
+import { IAuthDocument, IAuthResponse, IResetPassword, ISignInPayload, ISignUpPayload } from 'src/features/auth/interfaces/auth.interface';
+import { IBuyerDocument } from 'src/features/buyer/interfaces/buyer.interface';
+import { IConversationDocument, IMessageDocument } from 'src/features/chat/interfaces/chat.interfaces';
+import { ICreateGig, ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
+import { IOrderDocument, IOrderNotifcation } from 'src/features/order/interfaces/order.interface';
+import { IReviewDocument } from 'src/features/order/interfaces/review.interface';
+import { IEducation, IExperience, ILanguage, IPersonalInfoData, ISellerDocument } from 'src/features/sellers/interfaces/seller.interface';
 
 export interface ValidationErrorType {
   path: string;
@@ -12,6 +18,16 @@ export interface IDynamic {
   [key: string]: string;
 }
 
+export type validationErrorsType =
+  | ISignInPayload
+  | ISignUpPayload
+  | IResetPassword
+  | ICreateGig
+  | IPersonalInfoData
+  | IExperience
+  | IEducation
+  | ILanguage;
+
 export interface IQueryResponse {
   data: IAuthResponse;
   error: FetchBaseQueryError | SerializedError;
@@ -21,11 +37,24 @@ export interface IResponse {
   message?: string;
   token?: string;
   user?: IAuthDocument;
+  buyer?: IBuyerDocument;
+  seller?: ISellerDocument;
+  sellers?: ISellerDocument[];
+  gig?: ISellerGig;
+  gigs?: ISellerGig[];
   total?: number;
   sortItems?: string[];
+  conversations?: IConversationDocument[] | IMessageDocument[];
+  messages?: IMessageDocument[];
+  messageData?: IMessageDocument;
   conversationId?: string;
   clientSecret?: string;
   paymentIntentId?: string;
+  order?: IOrderDocument;
+  orders?: IOrderDocument[];
+  review?: IReviewDocument;
+  reviews?: IReviewDocument[];
+  notifications?: IOrderNotifcation[];
   browserName?: string;
   deviceType?: string;
 }
