dc3847763aff154f722cf54c1353b9c4e7a67748
dt4real <dotchere95@gmail.com>
2024-06-06 11:17:46 +0200

    Implement FeaturedExpects and HomeGigs components

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -1,11 +1,15 @@
 import { FC, ReactElement } from 'react';
 
+import FeaturedExperts from './FeaturedExperts';
+import HomeGigsView from './HomeGigsView';
 import HomeSlider from './HomeSlider';
 
 const Home: FC = (): ReactElement => {
   return (
     <div className="m-auto px-6 w-screen relative min-h-screen xl:container md:px-12 lg:px-6">
       <HomeSlider />
+      <HomeGigsView gigs={[]} title="Because you viewed a gig on" subTitle="" category="Programming & Tech" />
+      <FeaturedExperts sellers={[]} />
     </div>
   );
 };
--- src/features/home/interfaces/home.interface.ts
+++ src/features/home/interfaces/home.interface.ts
@@ -1,3 +1,6 @@
+import { ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
+import { ISellerDocument } from 'src/features/sellers/interfaces/seller.interface';
+
 export interface ISliderState {
   slideShow: string;
   slideIndex: number;
@@ -6,4 +9,15 @@ export interface ISliderState {
 export interface ICategory {
   name: string;
   icon: string;
+}
+
+export interface IHomeProps {
+  gigs: ISellerGig[];
+  title: string;
+  subTitle?: string;
+  category?: string;
+}
+
+export interface IFeaturedExpertProps {
+  sellers: ISellerDocument[];
 }
